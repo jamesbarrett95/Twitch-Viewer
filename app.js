10168328c6ce812cc521662bd5c850c84200749b
BBD\james.barrett <james.barrett@brightblueday.com>
2017-01-23 17:33:43 +0000

    Structure compelete, JavaScript to be added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-const url = "https://api.twitch.tv/kraken/streams/" + channels[] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&callback=?";
+for(let i = 0; i < channels.length; i++) {
+  let url = "https://api.twitch.tv/kraken/streams/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&callback=?";
 
-function callback(data) {
-  console.log(data);
+  function callback(data) {
+    console.log(data);
+  }
+
+  $.getJSON(url, callback);
+
 }
-
-$.getJSON(url, callback);
