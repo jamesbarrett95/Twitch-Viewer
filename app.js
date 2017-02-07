58e485dde3bb15496753556c66cf0241cbf2ae4b
BBD\james.barrett <james.barrett@brightblueday.com>
2017-02-07 15:30:42 +0000

    SASS Files Introduced to project and Twitch channels retrieved

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+const streamers = document.getElementById("streamers");
+
 for(let i = 0; i < channels.length; i++) {
-  let url = "https://api.twitch.tv/kraken/streams/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&callback=?";
+  let url = "https://api.twitch.tv/kraken/channels/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&stream_type=all&callback=?";
+  let channel = channels[i];
 
   function callback(data) {
     console.log(data);
+    const a = document.createElement("a");
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const img = document.createElement("img");
+
+    a.setAttribute("href", data.url)
+    a.setAttribute("target", "blank");
+    a.appendChild(li);
+    img.setAttribute("src", data.logo);
+    li.appendChild(img);
+    span.textContent = data.display_name;
+    li.appendChild(span);
+    streamers.appendChild(a);
   }
 
   $.getJSON(url, callback);
-
 }
