500936e6bca3bc3494c97b95d7bfbda5838e71a1
BBD\james.barrett <james.barrett@brightblueday.com>
2017-02-28 17:33:10 +0000

    Work Commit - Streamer information retrieved and to be completed

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-const streamersChildren = streamers.children;
-let promise = Promise.resolve();
+"use strict";
 
-for(let i = 0; i < channels.length; i++) {
-  let url = "https://api.twitch.tv/kraken/channels/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&stream_type=all&callback=?";
-  const p = $.getJSON(url);
-  promise = promise.then(x => p).then(callback);
+var channels = ["efragtv", "OgamingSC2", "cretetion", "freecodecamp", "CohhCarnage", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var streamers = document.getElementById("streamers");
+var getStreamersPromise = Promise.resolve();
+var getStreamerInfoPromise = Promise.resolve();
+
+var getStreamers = function(i) {
+  var url = "https://api.twitch.tv/kraken/channels/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&stream_type=all&callback=?";
+  var p = $.getJSON(url);
+  getStreamersPromise = getStreamersPromise.then(function () {
+    return p;
+  }).then(getStreamersCallback);
+};
+
+var getStreamerInfo = function(i) {
+  var url = "https://api.twitch.tv/kraken/streams/" + channels[i] + "?client_id=t300383ams5iuxlej34gzwk11qjepn&stream_type=all&callback=?";
+  var p = $.getJSON(url);
+  getStreamerInfoPromise = getStreamerInfoPromise.then(function () {
+    return p;
+  }).then(getStreamerInfoCallback);
 }
 
-  function callback(data) {
-    const a = document.createElement("a");
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const img = document.createElement("img");
+function getStreamersCallback(data) {
+  var a = document.createElement("a");
+  var li = document.createElement("li");
+  var span = document.createElement("span");
+  var img = document.createElement("img");
 
-    a.setAttribute("href", data.url)
-    a.setAttribute("target", "blank");
-    a.appendChild(li);
-    img.setAttribute("src", data.logo);
-    li.appendChild(img);
-    span.textContent = data.display_name;
-    li.appendChild(span);
-    streamers.appendChild(a);
-  }
+  a.setAttribute("href", data.url);
+  a.setAttribute("target", "blank");
+  a.appendChild(li);
+  img.setAttribute("src", data.logo);
+  li.appendChild(img);
+  span.textContent = data.display_name;
+  li.appendChild(span);
+  streamers.appendChild(a);
+}
+function getStreamerInfoCallback(data) {
+  console.log(data);
+}
+
+for (var i = 0; i < channels.length; i++) {
+  getStreamers(i);
+}
+
+for (var i = 0; i < channels.length; i++) {
+  getStreamerInfo(i);
+}
