7aaa3c1761a87e2f04f8dfd6920c8e21b8f1fa7e
BBD\james.barrett <james.barrett@brightblueday.com>
2017-03-07 16:57:08 +0000

    Bug fixes and SASS amends

--- app.js
+++ app.js
@@ -13,21 +13,41 @@ var streamers = document.getElementById("streamers");
 var getStreamersPromise = Promise.resolve();
 
 function appendOfflineUser(data) {
+  var row = document.createElement("div");
+  var leftCol = document.createElement("div");
+  var rightCol = document.createElement("div");
   var a = document.createElement("a");
   var li = document.createElement("li");
-  var span = document.createElement("span");
+  var name = document.createElement("span");
+  var offline = document.createElement("small");
   var img = document.createElement("img");
+
+  row.className = "offline all";
+
+  leftCol.className = "leftCol";
+  rightCol.className = "rightCol";
 
   a.setAttribute("href", data.url);
   a.setAttribute("target", "blank");
-  a.className = "offline";
-  a.className += " all";
-  a.appendChild(li);
+
   img.setAttribute("src", data.logo);
+
+  name.textContent = data.display_name;
+
   li.appendChild(img);
-  span.textContent = data.display_name;
-  li.appendChild(span);
-  streamers.appendChild(a);
+  li.appendChild(name);
+  a.appendChild(li);
+  leftCol.appendChild(a);
+
+  offline.textContent = "Offline";
+
+  rightCol.appendChild(offline);
+
+  row.appendChild(leftCol);
+  row.appendChild(rightCol);
+
+  streamers.appendChild(row);
+
 }
 
 function getOfflineUser(offlineUser) {
@@ -42,21 +62,41 @@ function getStreamersCallback(data) {
   } else {
     var onlineUser = data._links.channel.substr(38);
     var url = "https://twitch.tv/" + onlineUser;
+
+    var row = document.createElement("div");
+    var leftCol = document.createElement("div");
+    var rightCol = document.createElement("div");
     var a = document.createElement("a");
     var li = document.createElement("li");
-    var span = document.createElement("span");
+    var name = document.createElement("span");
+    var online = document.createElement("small");
     var img = document.createElement("img");
+
+    row.className = "online all";
+
+    leftCol.className = "leftCol";
+    rightCol.className = "rightCol";
 
     a.setAttribute("href", url);
     a.setAttribute("target", "blank");
-    a.className = "online";
-    a.className += " all";
-    a.appendChild(li);
+
     img.setAttribute("src", data.stream.channel.logo);
+
+    name.textContent = data.stream.channel.display_name;
+
     li.appendChild(img);
-    span.textContent = data.stream.channel.display_name;
-    li.appendChild(span);
-    streamers.appendChild(a);
+    li.appendChild(name);
+    a.appendChild(li);
+    leftCol.appendChild(a);
+
+    online.textContent = "Online";
+
+    rightCol.appendChild(online);
+
+    row.appendChild(leftCol);
+    row.appendChild(rightCol);
+
+    streamers.appendChild(row);
   }
 }
 
@@ -93,6 +133,7 @@ offline.addEventListener("click", () => {
 });
 
 all.addEventListener("click", () => {
+  console.log(allUsers);
   for (var i = 0; i < allUsers.length; i++) {
     allUsers[i].style.display = "inline";
   }
